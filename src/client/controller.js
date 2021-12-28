6835f17657aa11b2e5d68f1df106d3a69901a1fd
Eric Rosenzweig <errosenzweig@ucsd.edu>
2021-12-27 20:55:14 -0800

    modified hud-gamepad library to support emitting events when buttons pushed/released and stick moved.

--- src/client/controller.js
+++ src/client/controller.js
@@ -1,21 +1,29 @@
-import { GamePad, MultiKey} from "../../node_modules/hud-gamepad";
+import { MultiKey } from "../../node_modules/hud-gamepad";
+import { GamePad } from "./gamepad";
 
 GamePad.setup({
-    canvas:"controller",
-    start:{name:"start", key:"b"},
-    select:{name:"select", key:"v"},
-    trace:true,
-    debug:true,
-    hint:true,
-    buttons:[
-      {name:"a", "key":"d"},
-      {name:"b", "key":"s"},
-      {name:"x", "key":"w"},
-      {name:"y", "key":"a"}
+    canvas: "controller",
+    start: {name: "start", key: "b"},
+    select: {name: "select", key: "v"},
+    trace: true,
+    debug: true,
+    hint: true,
+    buttons: [
+      {name: "a", "key": "d"},
+      {name: "b", "key": "s"},
+      {name: "x", "key": "w"},
+      {name: "y", "key": "a"}
     ]
 });
 
-MultiKey.setup(GamePad.events, "wasdbv", true );
+MultiKey.setup(GamePad.events, "wasd", true );
 
+GamePad.cEvents.on("a-touchstart", function(e) {
+  console.log("a was pressed: ");
+  console.log(e);
+});
 
-
+GamePad.cEvents.on("stick-touchmove", function(e) {
+  console.log("got stick event: ");
+  console.log(e);
+})
